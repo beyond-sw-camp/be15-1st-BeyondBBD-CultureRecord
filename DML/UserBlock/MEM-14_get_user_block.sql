3d240ca304231cfc9577a378bbcd2a0ad94a786c
kimjy8937 <kimjy3520@gmail.com>
2025-02-12 21:45:02 +0900

    [feat #3] 차단 기능 구현

---
+++ DML/UserBlock/MEM-14_get_user_block.sql
@@ -0,0 +1,16 @@
+/*
+ * [MEM-15] 차단 목록 조회
+ * : SELECT, 사용자 ID로 자신이 차단한 유저 ID와 닉네임, 차단한 시각을 차단한 시각 순으로 조회
+*/
+
+SELECT 
+	u.user_id AS '차단한 유저 닉네임'
+	,u.nickname AS '차단당한 유저 닉네임'
+	,blocked_at AS '차단한 날짜'
+	FROM block_user b
+	JOIN user u ON b.`blocked_user_id`= u.user_id
+	WHERE blocker_id = 1
+	ORDER BY blocked_at;
+
+
+
