ea3454c116a2586feb160086749b5d1ff95e8d85
kimjy8937 <kimjy3520@gmail.com>
2025-02-13 16:41:20 +0900

    [refactor #13] 사용자 정지 트리거 중복코드 삭제

--- Trigger/TRI-04_user_ban_check.sql
+++ Trigger/TRI-04_user_ban_check.sql
@@ -44,45 +44,4 @@ BEGIN
     END IF;
 END//
 
-DELIMITER ;DELIMITER //
-
-CREATE TRIGGER user_ban_check               
-AFTER UPDATE ON report_check
-FOR EACH ROW
-BEGIN
-    DECLARE count_checked INT;
-    DECLARE target_user_id INT;
-
-    IF NEW.is_checked = 1 AND OLD.is_checked = 0 THEN
-        
-        IF NEW.report_type = 'post' THEN
-            SELECT COUNT(*) INTO count_checked
-            FROM report_check r
-            JOIN post p ON r.post_id = p.post_id
-            WHERE p.user_id = (SELECT user_id FROM post WHERE post_id = NEW.post_id)
-              AND r.is_checked = 1;
-
-        
-        ELSEIF NEW.report_type = 'comment' THEN
-            SELECT COUNT(*) INTO count_checked
-            FROM report_check r
-            JOIN comment c ON r.comment_id = c.comment_id
-            WHERE c.user_id = (SELECT user_id FROM comment WHERE comment_id = NEW.comment_id)
-              AND r.is_checked = 1;
-        END IF;
-
-        
-        IF count_checked >= 3 THEN
-            IF NEW.report_type = 'post' THEN
-                SELECT user_id INTO target_user_id FROM post WHERE post_id = NEW.post_id;
-            ELSEIF NEW.report_type = 'comment' THEN
-                SELECT user_id INTO target_user_id FROM comment WHERE comment_id = NEW.comment_id;
-            END IF;
-
-            INSERT INTO banned_user (banned_user_id, ban_reason) 
-            VALUES (target_user_id, '신고 누적');      
-        END IF;
-    END IF;
-END//
-
 DELIMITER ;
