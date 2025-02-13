f40c96b74bf1686c03382769d685d0a8bde12050
kimjy8937 <kimjy3520@gmail.com>
2025-02-13 20:52:01 +0900

    [refactor #12] 공연/전시 상세조회 출연진 한번에 조회 가능하도록 수정

--- DML/EventDetailView/PER-09_event_view_detail.sql
+++ DML/EventDetailView/PER-09_event_view_detail.sql
@@ -9,6 +9,7 @@ SELECT
     e.event_id AS '공연/전시 ID'
     , e.title AS '제목'
     , e.location AS '관람 장소'
+    , e.poster_image_url AS '포스터 이미지'
     , e.event_start_date AS '관람 시작일'
     , e.event_finish_date AS '관람 종료일'
     , e.running_time AS '관람 시간'
@@ -17,34 +18,14 @@ SELECT
     , e.description AS '설명'
     , e.event_status AS '공연 상태'
     , e.sales_status AS '판매 상태'
-    , e.sales_start_date AS '판매 시작일'
-    , e.sales_finish_date AS '판매 종료일'
-    , e.poster_image_url AS '포스터 이미지 주소'
-    , e.review_count AS '총 리뷰 수'
-    , CASE 
-        WHEN e.review_count > 0 THEN ROUND(e.review_rating_sum / e.review_count, 1)
-        ELSE 0
-    END AS avg_rating
-FROM event e
-JOIN show_cast sc ON e.event_id = sc.show_id
-JOIN artist a ON sc.artist_id = a.artist_id
-WHERE e.event_id = 2; -- 공연/전시 ID 입력
-
-
-/*
- * [PER-XX] 공연/전시 별 캐스트 조회
- * : SELECT,show_cast 테이블에서 event 테이블의 특정 공연/전시 ID로 검색한 캐스트의 목록에 대해 조회 
- */
-SELECT 
-    a.artist_id,
-    a.artist_name,
-    a.artist_job,
-    a.artist_age,
-    a.artist_gender,
-    a.profile_image_url,
-    sc.cast
-FROM show_cast sc
-JOIN artist a ON sc.artist_id = a.artist_id
-WHERE sc.show_id = 1; -- 공연/전시 ID 를 조회
-
+    , e.sales_start_date '판매 시작일'
+    , e.sales_finish_date '판매 종료일'
+    , f.field_name AS '분야'
+    ,GROUP_CONCAT(a.artist_name SEPARATOR ', ') AS '출연진'
+    ,ROUND(e.review_rating_sum / NULLIF(e.review_count, 0), 1) AS '평균별점'
+    FROM `event` e
+    JOIN show_cast s ON s.show_id = e.event_id
+    JOIN field f ON e.type_id = f.field_id
+    JOIN artist a ON s.artist_id = a.artist_id
+    WHERE e.event_id = 1;
 
